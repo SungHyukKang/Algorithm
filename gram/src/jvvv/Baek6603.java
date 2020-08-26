830b595091f2c565dd681b01a363fac4771f1070
성혁 강 <95line@naver.com>
2020-08-26 18:23:25 +0900

    Baek6603 / 로또 , 백트래킹

--- gram/src/jvvv/Baek6603.java
+++ gram/src/jvvv/Baek6603.java
@@ -2,45 +2,46 @@ package jvvv;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Stack;
 import java.util.StringTokenizer;
 public class Baek6603 {
-	 static void print(int[] arr, boolean[] visited, int n) {
-	        for (int i = 0; i < n; i++) {
-	            if (visited[i]) {
-	                System.out.print(arr[i] + " ");
-	            }
-	        }
-	        System.out.println();
-	    }
-	 
-	static void combination(int[] arr, boolean[] visited, int start, int n, int r) {
-	    if(r == 0) {
-	        print(arr, visited, n);
-	        return;
-	    } 
-
-	    for(int i=start; i<n; i++) {
-	        visited[i] = true;
-	        combination(arr, visited, i + 1, n, r - 1);
-	        visited[i] = false;
-	    }
+	public static int[] arr ;
+	public static boolean[] visited ;
+	public static int N;
+	public static Stack<Integer> stack = new Stack<>();
+	public static void dfs() {
+		if(stack.size()==7) {
+			for(int i =1;i<stack.size();i++) {
+				System.out.print(stack.get(i)+" ");
+			}
+			System.out.println();
+			return;
+		}
+		for(int i =1;i<=N;i++) {
+			if(!visited[i]&&stack.lastElement()<arr[i]) {
+				visited[i]=true;
+				stack.push(arr[i]);
+				dfs();
+				stack.pop();
+				visited[i]=false;
+			}
+		}
 	}
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int[] arr ;
-		boolean[] visited ;
 		while(true){
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			int N = Integer.parseInt(st.nextToken());
+			N = Integer.parseInt(st.nextToken());
+			stack = new Stack<>();
 			if(N==0)
 				return;
-			arr=new int[N];
-			visited=new boolean[N];
-			for(int i =0;i<N;i++){
+			arr=new int[N+1];
+			visited=new boolean[N+1];
+			for(int i =1;i<=N;i++) {
 				arr[i]=Integer.parseInt(st.nextToken());
 			}
-			combination(arr,visited,0,N,6);
+			stack.push(-1);
+			dfs();
 			System.out.println();
 		}
 	}
