05eab103a8cacef219bd1f45903adfc2ceb25992
성혁 강 <95line@naver.com>
2020-08-27 18:36:43 +0900

    프로그래머스 // 네트워크 , DFS

--- gram/src/Search/Network.java
+++ gram/src/Search/Network.java
@@ -1,15 +1,35 @@
 package Search;
-
 public class Network {
-
+	public static int size;
+	public static boolean[] visited;
+	public static int count =0 ;
+	public static void dfs(int[][] arr,int start) {
+		for(int i =0;i<size;i++) {
+			if(arr[start][i]==1&&!visited[i]&&i!=start) {
+				visited[i]=true;
+				dfs(arr,i);
+			}
+		}
+	}
+	
 	public int solution(int n , int[][] computers){
-		return 0;
+		size = n;
+		visited=new boolean[n];
+		for(int i =0;i<n;i++) {
+			if(!visited[i]) {
+				visited[i]=true;
+				dfs(computers,i);
+				count++;
+			}
+		}
+		System.out.println(count);
+		return count;
 	}
 	
 	public static void main(String[] args) {
 			
 		Network n = new Network();
-		n.solution(3, new int[][]{{1,1,0},{1,1,0},{0,0,1}});
+		n.solution(4, new int[][]{{1,0,0,0},{0,1,0,0},{0,0,1,0},{0,0,0,1}});
 
 	}
 
