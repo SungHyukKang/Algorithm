96875d97c78049df5295129ab060d239faa9b944
성혁 강 <95line@naver.com>
2020-08-28 18:20:55 +0900

    백준 2512 / 예산 , 이분탐색

---
+++ gram/src/Search/WordConvert.java
@@ -0,0 +1,14 @@
+package Search;
+
+public class WordConvert {
+	public int solution(String begin, String target, String[] words) {
+        int answer = 0;
+        return answer;
+    }
+	
+	public static void main(String[] args) {
+		WordConvert wc = new WordConvert();
+		wc.solution("hit", "cog", new String[] {"hot","dot","dog","lot","log","cog"});
+	}
+
+}
