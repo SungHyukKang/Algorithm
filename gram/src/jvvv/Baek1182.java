b7bf135d821c901a2ab42d2b32e8713f832a80b9
성혁 강 <95line@naver.com>
2020-08-31 19:38:52 +0900

    백준 1182 / 부분수열의 합 , 백트래킹 .

--- gram/src/jvvv/Baek1182.java
+++ gram/src/jvvv/Baek1182.java
@@ -1,21 +1,13 @@
 package jvvv;
-
+import java.util.Arrays;
 import java.util.Scanner;
-import java.util.Stack;
-
 public class Baek1182 {
-
 	public static int N;
 	public static int S;
 	public static int[] arr;
-	public static boolean[] visited;
-	public static int count=0;
-	public static Stack<Integer> stack=new Stack<>();
-	public static int sum = 0;
 	public static int cnt=0;
 	public static void main(String[] args) {
 		//백준 1182 , 부분수열의 합 //백트래킹
-		
 		Scanner sc = new Scanner(System.in);
 		N = sc.nextInt();
 		S = sc.nextInt();
@@ -23,13 +15,21 @@ public class Baek1182 {
 		for(int i=0;i<N;i++) {
 			arr[i]=sc.nextInt();
 		}
-		visited = new boolean[N];
-		dfs();
-		System.out.println(count);
+		Arrays.sort(arr);
+		dfs(0,0);
+		if(S==0)
+			cnt--;
+		System.out.println(cnt);
+	}
+	public static void dfs(int start , int sum) {
+		if(start==N) {
+			if(sum==S)
+				cnt++;
+			return;
+		}
+		dfs(start+1,sum+arr[start]);
+		dfs(start+1,sum);
 	}
 	
-	public static void dfs() {
-		
-	}
-
+	
 }
