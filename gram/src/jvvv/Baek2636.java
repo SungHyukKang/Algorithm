be6bfc0eb1246eff56ac2fa3fc361a9627ea7da0
성혁 강 <95line@naver.com>
2020-08-31 19:37:20 +0900

    백준 2636 / 치즈 , BFS

--- gram/src/jvvv/Baek2636.java
+++ gram/src/jvvv/Baek2636.java
@@ -12,7 +12,9 @@ public class Baek2636 {
 	public static int[][] arr;
 	public static boolean[][] visited;
 	public static int count=0;
-	public static Queue<Pair> queue= new LinkedList<>();;
+	public static Queue<Pair> queue= new LinkedList<>();
+	public static Queue<Pair> queue2= new LinkedList<>();
+	public static Queue<Pair> queue3= new LinkedList<>();
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		N = sc.nextInt();
@@ -23,36 +25,96 @@ public class Baek2636 {
 			for(int j=0;j<M;j++) {
 				int num =sc.nextInt();
 				arr[i][j]=num;
-				if(arr[i][j]==1) {
-					queue.offer(new Pair(i,j));
+			}
+		}
+		
+		int lastc=0;
+		while(true) {
+			
+			visited = new boolean[N][M];
+			func();
+			for(int i = 0;i<N;i++) {
+				for(int j =0;j<M;j++) {
+					if(arr[i][j]==1) {
+						queue2.offer(new Pair(i,j));
+					}
 				}
 			}
-		}
-		bfs();
-		
-		for(int i =0;i<N;i++) {
-			for(int j =0;j<M;j++) {
-				System.out.print(arr[i][j]+" ");
-				
+			bfs();
+			if(queue3.isEmpty())
+				break;
+			lastc=queue3.size();
+			while(!queue3.isEmpty()) {
+				Pair pair = queue3.poll();
+				arr[pair.x][pair.y]=-1;
 			}
-			System.out.println(); 
-			
-		}
+			count++;
+			for(int i =0;i<arr.length;i++) {
+				for(int j =0;j<arr[i].length;j++) {
+					if(arr[i][j]==-1)
+					System.out.print(arr[i][j] + " ");
+					else
+						System.out.print(" "+arr[i][j] + " ");
+				}
+				System.out.println();
+			}
+		System.out.println("-----------------------------------------------------");
+		}		
 		
 		System.out.println(count);
-		
+		System.out.println(lastc);
 	}
 	public static void bfs() {
-		
-	}
-	public static boolean isPossible(int x, int y) {
-		if(x<0||x>=N||y<0||y>=M){
-            return false;
-        }
-		if(arr[x][y]==1||!visited[x][y]) {
-			visited[x][y]=true;
-			return true;
+		while(!queue2.isEmpty()) {
+			Pair pair = queue2.poll();
+			visited[pair.x][pair.y]=true;
+			if(arr[pair.x+1][pair.y]==-1) {
+				arr[pair.x][pair.y]=2;
+				queue3.offer(new Pair(pair.x,pair.y));
+			}else if(arr[pair.x-1][pair.y]==-1) {
+				arr[pair.x][pair.y]=2;
+				queue3.offer(new Pair(pair.x,pair.y));
+			}else if(arr[pair.x][pair.y+1]==-1) {
+				arr[pair.x][pair.y]=2;
+				queue3.offer(new Pair(pair.x,pair.y));
+			}else if(arr[pair.x][pair.y-1]==-1) {
+				arr[pair.x][pair.y]=2;
+				queue3.offer(new Pair(pair.x,pair.y));
+			}
 		}
-		return false;
+	}
+	
+	public static boolean isPossible(int x, int y) {
+		if(x<0||x>=N||y<0||y>=M) {
+			return false;
+		}
+		return true;
+	}
+	
+	public static void func() {
+		visited=new boolean[N][M];
+		queue.offer(new Pair(0,0));
+		visited[0][0]=true;
+		arr[0][0]=-1;
+		while(!queue.isEmpty()) {
+			Pair pair = queue.poll();
+			if(isPossible(pair.x+1,pair.y)&&arr[pair.x+1][pair.y]!=1&&!visited[pair.x+1][pair.y]) {
+				visited[pair.x+1][pair.y]=true;
+				arr[pair.x+1][pair.y]=-1;
+				queue.offer(new Pair(pair.x+1,pair.y));
+			}if(isPossible(pair.x-1,pair.y)&&arr[pair.x-1][pair.y]!=1&&!visited[pair.x-1][pair.y]) {
+				visited[pair.x-1][pair.y]=true;
+				arr[pair.x-1][pair.y]=-1;
+				queue.offer(new Pair(pair.x-1,pair.y));
+			}if(isPossible(pair.x,pair.y+1)&&arr[pair.x][pair.y+1]!=1&&!visited[pair.x][pair.y+1]) {
+				visited[pair.x][pair.y+1]=true;
+				arr[pair.x][pair.y+1]=-1;
+				queue.offer(new Pair(pair.x,pair.y+1));
+			}if(isPossible(pair.x,pair.y-1)&&arr[pair.x][pair.y-1]!=1&&!visited[pair.x][pair.y-1]){
+				visited[pair.x][pair.y-1]=true;
+				arr[pair.x][pair.y-1]=-1;
+				queue.offer(new Pair(pair.x,pair.y-1));
+			}
+		}
 	}
 }
