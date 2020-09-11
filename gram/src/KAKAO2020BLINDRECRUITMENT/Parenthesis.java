8bfa052fdf5febfc43251c61e44283d80b1dc5be
성혁 강 <95line@naver.com>
2020-09-11 18:36:46 +0900

    프로그래머스 , 카카오 블라인드 채용 2020// 괄호 변환

--- gram/src/KAKAO2020BLINDRECRUITMENT/Parenthesis.java
+++ gram/src/KAKAO2020BLINDRECRUITMENT/Parenthesis.java
@@ -1,18 +1,54 @@
 package KAKAO2020BLINDRECRUITMENT;
 
-import java.util.ArrayList;
 
 public class Parenthesis {
-	StringBuffer answer = new StringBuffer();
-	public String solution(String p){
+	public static String func(String word) {
+		if(word.length()==0) {
+			return"";
+		}
+		int[] cnt = new int[2];
+		String u="";
+		String v="";
+		for(int i =0;i<word.length();i++) {
+			if((cnt[0]!=0&&cnt[1]!=0)&&cnt[0]==cnt[1]) {
+				break;
+			}
+			if(word.charAt(i)=='(') {
+				u+="(";
+				cnt[0]++;
+			}else {
+				u+=")";
+				cnt[1]++;
+			}
+		}
+		v=word.substring(u.length(),word.length());
+		if("".equals(u)||u.charAt(0)=='('&&u.charAt(u.length()-1)==')') {
+			return u+func(v);
+		}else {
+			String temp="("+func(v)+")";
+			u=u.substring(1,u.length()-1);
+			StringBuilder sb =new StringBuilder();
+			for(int i =0;i<u.length();i++) {
+				if(u.charAt(i)=='(') {
+					sb.append(')');
+				}else
+					sb.append('(');
+			}
+			return temp+sb.toString();
+		}
 		
-		return "";
+	}
+	
+	public String solution(String p){
+		if(p.equals(""))
+			return p;
+		return func(p);
 	}
 	
 	public static void main(String[] args) {
 		
 		Parenthesis pt =new Parenthesis();
-		pt.solution("()))((()");
+		System.out.println(pt.solution("(()())()"));
 		
 	}
 
