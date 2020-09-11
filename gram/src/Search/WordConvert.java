f85c9cfa213de3c2eda30e9e7c3de483bdef064b
성혁 강 <95line@naver.com>
2020-09-11 15:22:45 +0900

    프로그래머스 3단계  //단어변환 백트래킹 . BFS로 풀 수도 있나보다 .

--- gram/src/Search/WordConvert.java
+++ gram/src/Search/WordConvert.java
@@ -1,17 +1,71 @@
 package Search;
+import java.util.*;
 public class WordConvert {
-	
-	
-	
+	static Stack<String> stack = new Stack<>();
+	static String ans;
+	static String pre ;
+	static int count=0;
+	static boolean[] visited;
+	static String start;
+	static int min=2100000000;
 	public int solution(String begin, String target, String[] words) {
-		int answer = 0;
-        
-		return answer;
+        ans =target;
+        pre =begin;
+        start =begin;
+        visited= new boolean[words.length];
+        stack.push(begin);
+        for(int i =0;i<words.length;i++) {
+        	if(words[i].equals(begin)) {
+        		visited[i]=true;
+        		break;
+        	}
+        }
+        dfs(words);
+        if(min==2100000000) {
+//        	System.out.println(0);
+        	return 0;
+        }
+        System.out.println(min-1);
+		return min;
     }
+
+	public static void dfs(String[] words) {
+		if(pre.equals(ans)) {
+//			System.out.println("stack : "+stack+"\n pre : "+pre );
+			if(min>stack.size()) {
+				min=stack.size();
+			}
+			return;
+		}
+		for(int i =0;i<words.length;i++) {
+//			System.out.println("stack : "+stack+"\n pre : "+pre );
+			if(isPossible(words[i])&&!visited[i]) {
+				visited[i]=true;
+				pre=words[i];
+				stack.push(pre);
+				dfs(words);
+				stack.pop();
+				pre=stack.lastElement();
+				visited[i]=false;
+			}
+		}
+		
+	}
 	
+	public static boolean isPossible(String w) {
+		int cnt = 0;
+		for(int i =0;i<w.length();i++) {
+			if(w.charAt(i)!=pre.charAt(i)) {
+				cnt++;
+			}
+		}
+		if(cnt>1)
+			return false;
+		return true;
+	}
 	public static void main(String[] args) {
 		WordConvert wc = new WordConvert();
-		wc.solution("hit", "cog", new String[] {"hot","dot","dog","lot","log","cog"});
+		wc.solution("hit", "cog", new String[] {"hot", "dot", "dog", "lot", "log", "cog"});
 	}
 
 }
