44ebb6f9d3ddfa84b4615b249bf767b0cb621b32
성혁 강 <95line@naver.com>
2020-09-28 02:14:52 +0900

    백준 20005 //보스몬스터 전리품 , 다익스트라 , BFS
    BFS만 쓰면 될 것 같은데,
    한번 다익스트라도 써봤다.
    우선 각 플레이어들의 위치부터 보스몬스터의 위치의 최단거리를 모두 구한다.
    큐가 empty가 되면 반복문을 빠져나오는데 , 그때 보스몬스터의 위치에서 해당 플레이어의 최단 거리가
    INF가 아니라면 hsmapD변수에 플레이어의 이름 , 보스몬스터까지의 최단거리 꼴로 저장해주었다 .
    그리고 hsmap변수에 플레이어의 이름 , 데미지 꼴로 저장한 후
    61번째줄부터 72번째 줄 까지 처럼 반복문을 만들어 준 후
    출력해주면 답이 되었다.
    
    처음에 플레이어가 다른 플레이어의 위치로 갈 수 있는걸 고려하지 않고
    isPossible 함수에 arr[x][y]!=0 일 시에만 false를 날려줘서 삽질을 많이 했다.
    아오..

---
+++ gram/src/Baek20005.java
@@ -0,0 +1,130 @@
+import java.util.*;
+import java.io.*;
+
+
+class PairE{
+	int x,y;
+	PairE(int x, int y){
+		this.x=x;
+		this.y=y;
+	}
+}
+
+public class Baek20005 {
+	static int M;
+	static int N;
+	static int P;
+	static int[][] arr;
+	static PairE boss;
+	static int[][] D;
+	static int[] nx= {0,0,1,-1};
+	static int[] ny= {1,-1,0,0};
+	static HashMap<Character,Integer> hsmapD = new HashMap<>();
+	static final int INF=2100000000;
+	static ArrayList<PairE> list =new ArrayList<>(); 
+	public static int stoi(String X) {return Integer.parseInt(X);}
+	public static void main(String[] args)throws IOException {
+		BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		M = stoi(st.nextToken());
+		N = stoi(st.nextToken());
+		D=new int[M][N];
+		P = stoi(st.nextToken());
+		HashMap<Character,Integer> hsmap = new HashMap<>();
+		arr =new int[M][N];
+		for(int i =0;i<M;i++) {
+			String X =br.readLine();
+			for(int j =0;j<N;j++) {
+				D[i][j]=INF;
+				if(X.charAt(j)=='.') {
+					arr[i][j]=0;
+				}else if(X.charAt(j)=='X') {
+					arr[i][j]=-1;
+				}else if(X.charAt(j)=='B') {
+					boss = new PairE(i,j);
+				}else{
+					list.add(new PairE(i,j));
+					arr[i][j]=-(X.charAt(j));
+				}
+			}
+		}
+		for(int i =0;i<P;i++) {
+			st=new StringTokenizer(br.readLine());
+			char a = st.nextToken().charAt(0);
+			int xx = stoi(st.nextToken());
+			hsmap.put(a,xx);
+		}
+		dijkstra();
+		int HP = stoi(br.readLine());
+		int cnt =0;
+		while(true) {
+			if(HP<=0)
+				break;
+			for(char hero : hsmapD.keySet()) {
+				hsmapD.put(hero,hsmapD.get(hero)-1);
+				if(hsmapD.get(hero)==0)
+					cnt++;
+				if(hsmapD.get(hero)<=0) {
+					HP-=hsmap.get(hero);
+				}
+			}
+		}
+		System.out.println(cnt);
+	}
+	
+	
+	public static void dijkstra() {
+		for(int i =0;i<list.size();i++) {
+			PairE paire =list.get(i);
+			Queue<PairE> q = new LinkedList<PairE>();
+			q.offer(new PairE(paire.x,paire.y));
+			resetD();
+			D[paire.x][paire.y]=0;
+			while(!q.isEmpty()) {
+				PairE p = q.poll();
+				int x =p.x;
+				int y= p.y;
+				if(x==boss.x&&y==boss.y) {
+					break;
+				}
+				for(int j=0;j<nx.length;j++) {
+					int mx = x+nx[j];
+					int my = y+ny[j];
+					if(isPossible(mx,my)) {
+						if(D[mx][my]>D[x][y]+1) {
+							D[mx][my]=D[x][y]+1;
+							q.offer(new PairE(mx,my));
+						}
+					}
+					
+				}
+			}
+			if(D[boss.x][boss.y]!=INF)
+			hsmapD.put((char)-arr[paire.x][paire.y],D[boss.x][boss.y]);
+		}
+	}
+	public static boolean isPossible(int x, int y) {
+		if(x<0||x>=M||y<0||y>=N) {
+			return false;
+		}
+		if(arr[x][y]!=0&&arr[x][y]==-1)
+			return false;
+		return true;
+	}
+	public static void resetD() {
+		for(int i =0;i<M;i++) {
+			for(int j =0;j<N;j++) {
+				D[i][j]=INF;
+			}
+		}
+	}
+	public static void print() {
+		for(int i =0;i<M;i++) {
+			for(int j =0;j<N;j++) {
+				System.out.print(arr[i][j]);
+			}
+			System.out.println();
+		}
+	}
+}
