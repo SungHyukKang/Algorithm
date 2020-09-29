1a5704e512a2e082062d99364ccb998d2858a631
성혁 강 <95line@naver.com>
2020-09-29 17:11:02 +0900

    백준 1072 // 게임 , 이분탐색
    어렵다. 이분탐색...
    내 머리는 너무 나쁜 것 같다...
    열심히 해보자...

---
+++ gram/src/Baek1072.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+public class Baek1072 {
+
+	public static void main(String[] args)throws IOException {
+		BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		long X = Long.parseLong(st.nextToken());
+		long Y = Long.parseLong(st.nextToken());
+		long target = (Y*100/X)+1;
+		long cnt =0;
+		long left  = 1;
+		long right = 1000000000;
+		while(left<=right) {
+			long mid =(left+right)/2;
+			long val = (Y+mid)*100/(X+mid);
+			if(val>=target) {
+				right=mid-1;
+				cnt=mid;
+			}else
+				left=mid+1;
+		}
+		System.out.println(cnt);
+	}
+
+}
