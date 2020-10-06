8572b1c5160846f0e25586f8ec86c1fdcfada71a
성혁 강 <95line@naver.com>
2020-10-06 19:18:20 +0900

    백준 14889 // 스타트와 링크 , 백트래킹
    중복을 제거한 모든 가능한 팀의 조합을 구한 후
    각 팀의 능력치를 구한 후
    능력치의 차이의 최소를 변수에 저장하여 출력하였다.

--- gram/src/jvvv/Baek14889.java
+++ gram/src/jvvv/Baek14889.java
@@ -1,29 +1,70 @@
 package jvvv;
 
-import java.util.Scanner;
+import java.io.*;
 import java.util.Stack;
+import java.util.StringTokenizer;
 
-class Pair99{
-	
-}
 
 public class Baek14889 {
 	public static int N;
-	public static boolean[][] visited;
+	public static boolean[] visited;
+	public static Stack<Integer> stack = new Stack<>();
 	public static int[][] arr;
-	public static Stack<Pair> stack;
+	public static int max=9999999;
 	public static void dfs() {
+		if(stack.size()-1==N/2) {
+			int sum=0;
+			int sum2=0;
+			int ans=0;
+			for(int i =1;i<stack.size();i++) {
+				for(int j=1;j<stack.size();j++) {
+					if(stack.get(i)==stack.get(j))
+						continue;
+					sum+=arr[stack.get(i)][stack.get(j)];
+				}
+			}
+			for(int i =1;i<=N;i++) {
+				if(!visited[i]) {
+					for(int j=1;j<=N;j++) {
+						if(i==j)
+							continue;
+						if(!visited[j]) {
+							sum2+=arr[i][j];
+						}
+					}
+				}
+			}
+			ans = Math.abs(sum-sum2);
+			if(ans<max) {
+				max=ans;
+			}
+			return;
+		}
+		for(int i =1;i<=N;i++) {
+			if(!visited[i]&&stack.lastElement()<i) {
+				visited[i]=true;
+				stack.push(i);
+				dfs();
+				stack.pop();
+				visited[i]=false;
+			}
+		}
 		
 	}
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		N=sc.nextInt();
+	public static int stoi(String X) {return Integer.parseInt(X);}
+	public static void main(String[] args)throws IOException {
+		BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+		N=stoi(br.readLine());
 		arr =new int[N+1][N+1];
+		visited=new boolean[N+1];
+		stack.add(-1);
 		for(int i = 1;i<=N;i++){
+			StringTokenizer st=  new StringTokenizer(br.readLine());
 			for(int j =1;j<=N;j++){
-				arr[i][j]=sc.nextInt();
+				arr[i][j]=stoi(st.nextToken());
 			}
 		}
 		dfs();
+		System.out.println(max);
 	}
 }
