f8a317b4f43100b1b317641c9034e15f333b530b
성혁 강 <95line@naver.com>
2020-10-07 01:00:29 +0900

    백준 2960 // 에라토스테네스의 체  ,소수판정  ,구현

--- gram/src/jvvv/Baek2960.java
+++ gram/src/jvvv/Baek2960.java
@@ -1,5 +1,4 @@
 package jvvv;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Baek2960 {
@@ -7,6 +6,23 @@ public class Baek2960 {
         Scanner sc =new Scanner(System.in);
         int N = sc.nextInt();
         int M = sc.nextInt();
-
+        int[] arr = new int[N+1];
+        int cnt =0;
+        
+        for(int i=2;i<=N;i++) {
+        	for(int j =1;j<=N/2;j++) {
+        		if(i*j<=N) {
+        			if(arr[i*j]==-1)
+        				continue;
+        			arr[i*j]=-1;
+        			cnt++;
+        			if(cnt==M) {
+        				System.out.println(i*j);
+        				return;
+        			}
+        		}
+        	}
+        }
+        
     }
 }
