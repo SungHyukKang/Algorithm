eedce17ddf3829792286e750b559af8b3ef49464
성혁 강 <95line@naver.com>
2020-10-07 17:32:14 +0900

    백준 11725 // 트리의 부모 찾기 , DFS
    정점이 최대 100000개나 되기에 트리를 인접리스트를 통하여 저장하였다.
    DFS를 통하여 1번 정점부터 탐색하였다.
    인접리스트에서 뽑은 v1과 연결된 v2는  v1의자식 노드이므로
    v2의 부모노드를 v1으로 설정해준 후 방문처리하였다.

---
+++ gram/src/Baek11725.java
@@ -0,0 +1,48 @@
+import java.util.*;
+import java.io.*;
+public class Baek11725 {
+	static int N ;
+	static ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+	static int[] arr;
+	static int cnt=0;
+	static boolean[] visited;
+	public static int stoi(String X) {return Integer.parseInt(X);}
+	public static void main(String[] args)throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N=stoi(br.readLine());
+		arr=new int[N+1];
+		visited =new boolean[N+1];
+		for(int i =0;i<=N;i++) {
+			list.add(new ArrayList<>());
+		}
+		
+		for(int i =0;i<N-1;i++) {
+			StringTokenizer st =new StringTokenizer(br.readLine());
+			int v1 = stoi(st.nextToken());
+			int v2 = stoi(st.nextToken());
+			list.get(v1).add(v2);
+			list.get(v2).add(v1);
+		}
+		visited[1]=true;
+		dfs(1);
+		for(int i=2;i<=N;i++) {
+			System.out.println(arr[i]);
+		}
+		
+	}
+
+	public static void dfs(int start) {
+		if(cnt==N) {
+			return;
+		}
+		for(int X : list.get(start)) {
+			if(!visited[X]) {
+				visited[X]=true;
+				arr[X]=start;
+				dfs(X);
+				cnt++;
+			}
+		}
+	}
+	
+}
