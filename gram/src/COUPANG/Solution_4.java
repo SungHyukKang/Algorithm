24758f1613527aab86d1a6d62c2e4372eb42e357
성혁 강 <95line@naver.com>
2020-10-10 02:49:51 +0900

    쿠팡 2020 문제 4, 백트래킹
    1,3,4를 풀었으나 1,3은 코드 저장을 못했다.
    백트래킹을 이용하여 시작 위치부터 HUB까지 갈 수 있는 경우의 수를 구하고
    마찬가지로 똑같이 HUB에서 목적지까지 갈 수 있는 경우의 수를 구하여
    곱한 값을 10007로 나눈 나머지가 정답이 된다.
    정점이 양의 정수가 아니어서 방문처리를 어떻게 해야할 지 고민을 했는데,
    해시 맵을 사용하여 방문 처리해주었다.
    시간 초과가 관건인데 ... 모르겠다 . 신이시여

--- gram/src/COUPANG/Solution_4.java
+++ gram/src/COUPANG/Solution_4.java
@@ -5,6 +5,7 @@ class Solution_4 {
     static int destcnt=0;
     static String destination="";
     static String fndest;
+    static Stack<String> stack = new Stack<>();
     static HashMap<String,Integer> hsmap =new HashMap<>();
     public static int solution(String depar, String hub, String dest   , String[][] roads) {
         int answer = -1;
@@ -21,23 +22,29 @@ class Solution_4 {
             hsmap.put(roads[i][0],0);
             hsmap.put(roads[i][1],0);
         }
+//        System.out.println(hubcnt);
         destination = dest;
         dfs2(roads,hub);
+//        System.out.println(destcnt);
         long ans = hubcnt*destcnt;
         answer =(int)(ans%10007);
+//        System.out.println(answer);
         return answer;
     }
 
     public static void dfs(String[][] roads,String start){
         if(start.equals(destination)){
             hubcnt++;
+//            System.out.println(stack);
             return;
         }
         for(int i =0;i<roads.length;i++){
-            if(roads[i][1].equals(fndest))continue;
+            if(roads[i][1].equals(fndest)) continue;
             if(roads[i][0].equals(start)&&hsmap.get(start)==0){
                 hsmap.put(start,1);
+//                stack.push(start);
                 dfs(roads,roads[i][1]);
+//                stack.pop();
                 hsmap.put(start,0);
             }
         }   
@@ -46,21 +53,22 @@ class Solution_4 {
     public static void dfs2(String[][] roads,String start){
         if(start.equals(destination)){
             destcnt++;
+//            System.out.println(stack);
             return;
         }
         for(int i =0;i<roads.length;i++){
             if(roads[i][0].equals(start)&&hsmap.get(start)==0){
                 hsmap.put(start,1);
+//                stack.push(start);
                 dfs2(roads,roads[i][1]);
+//                stack.pop();
                 hsmap.put(start,0);
             }
         }   
     }
     
     public static void main(String[] args) {
-    	int Z =(200000*200000)%10007;
-    	long S = (200000*200000)%10007;
-    	System.out.println(Z);
-    	System.out.println(S);
+    	solution("a","g","z",new String[][] {{"a","b"},{"a","c"},{"a","e"},{"a","d"},{"a","h"},{"a","g"},{"e","b"},{"b","z"},{"b","g"},{"c","g"},{"d","c"},{"c","z"},{"g","z"}
+    	,{"g","x"},{"x","z"},{"g","y"},{"y","t"},{"t","l"},{"l","z"},{"y","l"}});
     }
 }
