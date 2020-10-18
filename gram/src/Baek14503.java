d6e76133cc616b642be6adbacc0c53627ea35b06
성혁 강 <95line@naver.com>
2020-10-18 18:00:04 +0900

    백준 14503 // 로봇 청소기 , BFS , 시뮬레이션
    현재 보고 있는 방향의 왼쪽부터 청소하는 청소기가 있다.
     북쪽은 0 , 동쪽은 1 , 남쪽은 2, 서쪽은 3
    현재 북쪽을 보고 있다면 움직여야 하는 위치는 서쪽인 3이다.
    그러므로
    0일 때는 3을 리턴해주는 함수를 만들어주면서 나머지의 경우도 위와 같은 방법으로 만든다.
    그렇게 nx와 ny를 상황에 맞게
    public static int[] nx = {0,1,0,-1};
    public static int[] ny = {-1,0,1,0};
    요런식으로 정해주고 ,
    청소할 곳이 아무 곳도 없다면 후진하는 로직도 짜고...
    하면 풀린다.
    근데 나는 당연히 로봇의 위치는 1,1 이상에서 시작인 줄 알고 짜서 거의 한시간 넘게 삽질했다.
    문제를 잘 읽자.

--- gram/src/Baek14503.java
+++ gram/src/Baek14503.java
@@ -1,5 +1,18 @@
 import java.util.*;
 import java.io.*;
+
+class Node14503{
+	int x;
+	int y;
+	int d;
+	
+	Node14503(int x, int y,int d){
+		this.x=x;
+		this.y=y;
+		this.d=d;
+	}
+}
+
 public class Baek14503 {
 	public static int N;
 	public static int M;
@@ -7,23 +20,83 @@ public class Baek14503 {
 	public static int c;
 	public static int d;
 	public static int[][] arr;
+	public static int[] nx = {0,1,0,-1};
+	public static int[] ny = {-1,0,1,0};
+	public static boolean[][] visited;
+	public static Queue<Node14503> q=new LinkedList<>();
 	public static void main(String[] args)throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		N=Integer.parseInt(st.nextToken());
 		M=Integer.parseInt(st.nextToken());
-		arr = new int[N+1][M+1];
+		arr = new int[N][M];
+		visited = new boolean[N][M];
 		st= new StringTokenizer(br.readLine());
 		r=Integer.parseInt(st.nextToken());
 		c=Integer.parseInt(st.nextToken());
 		d=Integer.parseInt(st.nextToken());
-		for(int i =1;i<=N;i++) {
+		q.offer(new Node14503(r,c,d));
+		visited[r][c]=true;
+		int ans =1;
+		for(int i =0;i<N;i++) {
 			st= new StringTokenizer(br.readLine());
-			for(int j =1;j<=M;j++) {
+			for(int j =0;j<M;j++) {
 				arr[i][j]=Integer.parseInt(st.nextToken());
 			}
+		}//0 북 , 1 동 , 2 남 ,3 서
+		while(!q.isEmpty()) {
+			Node14503 n = q.poll();
+			int x = n.x;
+			int y = n.y;
+			int d = n.d;
+			System.out.println(x+" "+y+ " "+d);
+			int cnt=0;
+			while(cnt<4) {
+				int dcnt=d+cnt;
+				if(dcnt>=nx.length) {
+					dcnt=Math.abs(nx.length-dcnt);
+				}
+				int mx = x+nx[dcnt];
+				int my = y+ny[dcnt];
+				if(isPossible(mx,my)&&!visited[mx][my]) {
+					dcnt = dcnt+1 > 3 ? 0 : dcnt+1;
+					q.offer(new Node14503(mx,my,dcnt));
+					visited[mx][my]=true;
+					ans++;
+					break;
+				}
+				cnt++;
+			}
+			if(q.isEmpty()) {
+				int mx=x;
+				int my=y;
+				if(d==0) {
+					mx += 1;
+				}else if(d==1) {
+					my+=1;
+				}else if(d==2) {
+					mx-=1;
+				}else if(d==3) {
+					my-=1;
+				}
+				if(isPossible(mx,my)) {
+					q.offer(new Node14503(mx,my,d));
+					if(!visited[mx][my]) {
+						visited[mx][my]=true;
+						ans++;
+					}
+				}
+			}
 		}
-		
+		System.out.println(ans);
 	}
-
+	
+	public static boolean isPossible(int x, int y) {
+		if(x<0||x>=N||y<0||y>=M) {
+			return false;
+		}
+		if(arr[x][y]==1)
+			return false;
+		return true;
+	}
 }
