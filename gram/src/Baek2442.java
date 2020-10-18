19cd1348001f90e6329ad6c614dc63199dcbb6ed
성혁 강 <95line@naver.com>
2020-10-18 23:50:00 +0900

    백준 2442/별찍기-5 , 구현
    중간고사 공부하다가 갑자기 생각나서 구현하고
    비슷한 문제에 제출해봤다.

---
+++ gram/src/Baek2442.java
@@ -0,0 +1,21 @@
+
+import java.util.*;
+public class Baek2442 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N =sc.nextInt()*2;
+		for(int i =0;i<N;i++) {
+			if(i%2==1) {
+				for(int k =0;k<(N-i)/2;k++) {
+					System.out.print(" ");
+				}
+				for(int j = 0;j<i;j++) {
+					System.out.print("*");
+				}
+				System.out.println();
+			}
+		}
+	}
+
+}
