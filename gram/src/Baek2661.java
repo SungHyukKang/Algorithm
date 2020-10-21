2dfd9c1ed14a3d903b86da2e9e9be42a693a49d6
성혁 강 <95line@naver.com>
2020-10-21 23:46:42 +0900

    백준 2661 // 좋은수열 , 백트래킹
    숫자 1, 2, 3으로만 이루어지는 수열이 있다. 임의의 길이의 인접한 두 개의 부분 수열이 동일한 것이 있으면,
    그 수열을 나쁜 수열이라고 부른다. 그렇지 않은 수열은 좋은 수열이다.
    백트래킹인데, substring을 사용하여 좋은 수열인지 나쁜 수열인지 판단해야한다.
    부분 수열 중 가장 긴 길이는 전체 수열 길이의 반이기 때문에
    2부터 전체수열 길이의 반까지 비교를 한다.(마지막 문자열과 같은 문자열은 if문에서 배제)
    예를 들어 ,수열 1213121을 판단할 때
    수열의 길이의 반은 3이기 때문에
    2개로 자른 수열끼리, 3개로 자른 수열끼리 비교하는데
    (3,5)와(5,7)을 비교하고
    (1,4)와(4,7)을 비교했을 때 같지 않다면
    좋은 수열인 것이다.

--- gram/src/Baek2661.java
+++ gram/src/Baek2661.java
@@ -1,10 +1,38 @@
-import java.util.*;
+import java.io.*;
 public class Baek2661 {
-
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		
+	static int N;
+	static StringBuilder sb=new StringBuilder(); 
+	
+	public static void main(String[] args)throws IOException {
+		BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+		N=Integer.parseInt(br.readLine());
+		sb.append(1);
+		dfs();
 	}
-
+	public static void dfs() {
+		if(sb.length()==N) {
+			System.out.println(sb);
+			System.exit(0);
+			return;
+		}
+		for(int i=1;i<=3;i++) {
+			if(!Character.toString(sb.toString().charAt(sb.toString().length()-1)).equals(String.valueOf(i))&&isPossible(sb.toString()+i)) {
+				sb.append(i);
+				dfs();
+				String X =sb.toString().substring(0,sb.length()-1);
+				sb=new StringBuilder(X);				
+			}
+		}
+	}
+	
+	public static boolean isPossible(String X) {
+		int num =X.length()/2;
+		for(int i=2;i<=num;i++) {
+			if(X.substring(X.length()-i,X.length()).equals(X.substring(X.length()-i-i,X.length()-i))) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
 }
