7fadac6adedc6375e01d4d24788317e7213226f9
성혁 강 <95line@naver.com>
2020-10-22 02:07:39 +0900

    백준 1541 // 잃어버린 괄호 , 그리디 ,문자열
    연산자가 + 일 경우 괄호를 씌워 준 계산식이 가장 작은 값이 나온다.
    이건 빨리 생각했는데
    구현이 좀 오래걸렸다.
    설명하기도 어려움

---
+++ gram/src/Baek1541.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.util.regex.Pattern;
+import java.io.*;
+public class Baek1541 {
+	public static void main(String[] args)throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String X =br.readLine();
+		String pattern = "^[0-9]*$";
+		ArrayList<Integer> list =new ArrayList<>();
+		ArrayList<Character> ope =new ArrayList<>();
+		StringTokenizer st= new StringTokenizer(X,"[+-]");
+		for(int i =0;i<X.length();i++) {
+			if(!Pattern.matches(pattern,Character.toString(X.charAt(i)))) {
+				list.add(Integer.parseInt(st.nextToken()));
+				ope.add(X.charAt(i));
+			}
+		}
+		list.add(Integer.parseInt(st.nextToken()));
+		int j=0;
+		for(int i =0;i<ope.size();i++) {
+			if(ope.get(i)=='+') {
+				list.set(j,list.get(j)+list.get(j+1));
+				list.remove(j+1);
+				j--;
+			}
+			j++;
+		}
+		int sum=list.get(0);
+		for(int i =1;i<list.size();i++) {
+			sum-=list.get(i);
+		}
+		System.out.println(sum);
+	}
+}
