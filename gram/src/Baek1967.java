ad57b7ce155e847dda21f7ffde7fed6d29808311
성혁 강 <95line@naver.com>
2020-12-16 17:45:34 +0900

    백준 1967 // 트리의 지름 , dfs
    두달 전에 포기했던 문제인데 다시 풀어봤다.
    간단했다.
    지름이란 원의 중심을 지나는 양 끝 점의 길이인데,
    간단하게 어떤 점에서 가장 멀리있는 다른 점의 길이이다.
    따라서, dfs를 이용하여 루트에서 가장 멀리 있는 노드를 찾고
    다시 그 노드에서 가장 먼 노드까지의 길이를 출력하면 되는 문제다.

--- gram/src/Baek1967.java
+++ gram/src/Baek1967.java
@@ -1,88 +1,78 @@
 import java.util.*;
 import java.io.*;
 
-class Node1967{
-	int i,d;
-	
-	Node1967(int i,int d){
-		this.i=i;
-		this.d=d;
+class Node1967 {
+	int i, d;
+
+	Node1967(int i, int d) {
+		this.i = i;
+		this.d = d;
 	}
 }
 
 public class Baek1967 {
 	static int N;
-	static int sofar=0;
-	static int pre;
-	static int max=0;
 	static boolean[] visited;
+	static int[] D;
 	static ArrayList<ArrayList<Node1967>> list = new ArrayList<>();
-	static ArrayList<ArrayList<Node1967>> ijc = new ArrayList<>();
-	public static int stoi(String X) {return Integer.parseInt(X);}
-	public static void main(String[] args)throws IOException {
+	public static int stoi(String X) {
+		return Integer.parseInt(X);
+	}
+
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N=stoi(br.readLine());
-		if(N==1) {
+		N = stoi(br.readLine());
+		if (N == 1) {
 			System.out.println("0");
 			return;
 		}
-		visited=new boolean[N+1];
-		for(int i =0;i<=N;i++) {
+		visited = new boolean[N + 1];
+		D = new int[N + 1];
+
+		for (int i = 0; i <= N; i++) {
 			list.add(new ArrayList<>());
-			ijc.add(new ArrayList<>());
+			D[i] = 21000000;
 		}
-		for(int i =0;i<N-1;i++) {
+		for (int i = 0; i < N-1; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			int num =stoi(st.nextToken());
-			int num2 =stoi(st.nextToken());
-			int num3 =stoi(st.nextToken());
-			list.get(num).add(new Node1967(num2,num3));
-			ijc.get(num2).add(new Node1967(num,num3));
-			ijc.get(num).add(new Node1967(num2,num3));
+			int s = stoi(st.nextToken());
+			int e = stoi(st.nextToken());
+			int d = stoi(st.nextToken());
+			list.get(s).add(new Node1967(e, d));
+			list.get(e).add(new Node1967(s, d));
 		}
-		
-		ArrayList<Integer> child = new ArrayList<>();
-		for(int i =1;i<list.size();i++) {
-			if(list.get(i).size()==0) {
-				child.add(i);
-			}
-		}
-		if(child.size()==1) {
-			dfs2(1,0);
-			System.out.println(max);
-			return;
-		}
-		visited[1]=true;
 		dfs(1,0);
-		max=0;
-		visited=new boolean[N+1];
-		dfs(sofar,0);
-		System.out.println(max);
-	}
-	public static void dfs2(int start,int sum) {
-			if(sum>max) {
-				max=sum;
-			}
-		for(Node1967 n : list.get(start)) {
-			if(!visited[n.i]) {
-				visited[n.i]=true;
-				sum+=n.d;
-				dfs2(n.i,sum);
-			}
-		}
+		D[1]=0;
 		
-	}
-	
-	public static void dfs(int start,int sum) {
-		if(max<sum) {
-			max=sum;
-			sofar=start;
+		int restart=0;
+		int dis=0;
+		for(int  i=1;i<D.length;i++) {
+			if(dis<D[i]) {
+				dis=D[i];
+				restart=i;
+			}
+			D[i]=21000000;
 		}
-		for(Node1967 n : ijc.get(start)) {
-			if(!visited[n.i]) {
-				visited[n.i]=true;
-				dfs(n.i,sum+n.d);
+		visited=new boolean[N+1];
+		dfs(restart,0);
+		D[0]=-1;
+		D[restart]=-1;
+		Arrays.sort(D);
+		System.out.println(D[D.length-1]);
+	}
+
+	static void dfs(int start, int sum) {
+		for (Node1967 n : list.get(start)) {
+			if (!visited[n.i]) {
+				visited[n.i] = true;
+				sum += n.d;
+				if(D[n.i]>sum)
+					D[n.i] = sum;
+				dfs(n.i, sum);
+				sum-=n.d;
+				visited[n.i]=false;
 			}
 		}
 	}
+
 }
