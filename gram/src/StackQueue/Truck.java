e549dfcf70343ad6d0933255d9ecbf37feb49ca3
성혁 강 <95line@naver.com>
2021-01-13 16:23:57 +0900

    프로그래머스 2단계 // 트럭 , 큐

--- gram/src/StackQueue/Truck.java
+++ gram/src/StackQueue/Truck.java
@@ -1,77 +1,45 @@
 package StackQueue;
+
+import java.util.*;
+
 public class Truck {
-	
-	class Node{
-		int time;
-		int weight;
-		Node next=null;
-		
-		Node(int t ,int w , Node n){
-			time=t;
-			weight=w;
-			next=n;
-		}
-	}
-	
-	class Queue{
-		Node head = null;
-		Node tail =null;
-		
-		Queue(int time ,int weight){
-			head = new Node(time,weight,null);
-			tail=head;
-		}
-		
-		Queue add(int time , int weight){
-			tail.next=new Node(time,weight,null);
-			tail=tail.next;
-			return this;
-		}
-		
-		void print(){
-			for(Node n = head;n!=null;n=n.next){
-				System.out.println(n.weight);
-			}
-		}
-		
-		Queue poll(){
-			if(head.next==null)
-				return this;
-			else{
-				head=head.next;
-				return this;
-			}
-		}
-	}
-	
-	
-	
 	public int solution(int bridge_length, int weight , int[] truck_weights){
 		int answer = 0;
-		Queue qu = null;
-		int x =0;
-		for(int i = 0;i<truck_weights.length;i++){
-			if(qu==null){
-				qu=new Queue(bridge_length, truck_weights[i]);
-				continue;
+		
+		Queue<Integer> q = new LinkedList<>();
+		int sum=0;
+		
+		for(int x : truck_weights) {
+			while(true) {
+				if(q.isEmpty()) {
+					q.offer(x);
+					sum+=x;
+					answer++;
+					break;
+				}else if(q.size()==bridge_length) {
+					sum-=q.poll();
+				}else {
+					if(sum+x>weight) {
+						q.offer(0);
+						answer++;
+					}else {
+						q.offer(x);
+						sum+=x;
+						answer++;
+						break;
+					}
+				}
 			}
-			qu.add(bridge_length, truck_weights[i]);
-			}
+		}
 		
-		qu.print();
-		
-		
-		
-		return answer;
+		return answer+bridge_length;
 	}
-	
+
 	public static void main(String[] args) {
 		Truck truck = new Truck();
-		
-		truck.solution(2, 10, new int[]{7,4,5,6});
+
+		truck.solution(2, 10, new int[] { 7, 4, 5, 6 });
 
 	}
-
-
 
 }
