26f21f1200a30a07662e1cceef7ff591a3183559
성혁 강 <95line@naver.com>
2021-01-19 15:16:13 +0900

    프로그래머스 2단계 // 문자열 압축 , 2020카카오 블라인드 채용 , 구현 , 문자열
    겨우 풀었다. 카카오 문자열 구현 문제는 너무 까다롭다 후우

--- gram/src/KAKAO2020BLINDRECRUITMENT/WordArchive.java
+++ gram/src/KAKAO2020BLINDRECRUITMENT/WordArchive.java
@@ -1,34 +1,56 @@
 package KAKAO2020BLINDRECRUITMENT;
+
 import java.util.*;
+
 public class WordArchive {
-	
+
 	public int solution(String s){
-		String t = null;
-		ArrayList<String> arr =new ArrayList<>();
-		StringBuilder sb = new StringBuilder();
-		LinkedHashMap<String,Integer> hsmap = new LinkedHashMap<>();
-		ArrayList<HashMap<String,Integer>> hsmapArr = new ArrayList<>();
-		arr.add(s);
-		String temp ;
-		int j =0;
-		for(int q = 2 ; q<s.length()/2;q++) {
-			sb=new StringBuilder();
-			hsmap=new LinkedHashMap<>();
-		for(int i =0;i<s.length()-q;i=i+q){
-			System.out.println("Q : "+q);
-			hsmap.put(s.substring(i,i+q),hsmap.getOrDefault(s.substring(i,i+q), 0)+1);
-			sb.append(s.substring(i,i+q));
-			System.out.println(hsmap);
+		int min = s.length();
+		for(int i =1;i<=s.length()/2;i++) {
+			StringBuilder sb=new StringBuilder();
+			int cnt=1;
+			String v="";
+			String cpy="";
+			for(int j =0;j<=s.length();j+=i) {
+				String X="";
+				if(j+i>s.length()) {
+					sb.append(s.substring(j));
+				}else {
+					 X = s.substring(j,j+i);
+					 if(v.equals(X)) {
+						 if(cnt==1) {
+							 String z  = sb.toString().substring(0,sb.length()-i);
+							 sb=new StringBuilder();
+							 sb.append(z);
+							 cpy=v;
+						 }
+						 cnt++;
+					 }else {
+						 if(cnt!=1) {
+							 sb.append(cnt+cpy+X);
+							 cnt=1;
+						 }else {
+							 sb.append(X);
+							 cnt=1;
+						 }
+					 }
+				}
+				v=X;
+			}
+			if(cnt!=1) {
+				sb.append(cnt+cpy);
+			}
+			min=Math.min(min,sb.length());
 		}
-		hsmap.put(s.substring(sb.toString().length(),s.length()),hsmap.getOrDefault(s.substring(sb.toString().length(),s.length()),0)+1);
-		hsmapArr.add(hsmap);
-		System.out.println("----------");
-		}
-		System.out.println(hsmapArr);
-		return 0;
+		return min;
 	}
-	public static void main(String[] args){
-		WordArchive wa = new WordArchive();				
+
+	public static void main(String[] args) {
+		WordArchive wa = new WordArchive();
+		wa.solution("aabbaccc");
+		wa.solution("ababcdcdababcdcd");
+		wa.solution("abcabcdede");
 		wa.solution("abcabcabcabcdededededede");
+		wa.solution("xababcdcdababcdcd");
 	}
 }
