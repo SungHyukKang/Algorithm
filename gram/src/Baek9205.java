5ab9aaededf1bfd04b0fdb0aa0c6768d82723e5d
성혁 강 <95line@naver.com>
2021-01-23 22:50:33 +0900

    백준 9205 // 맥주 마시면서 걸어가기 , BFS , 그래프
    굉장히 까다로운 문제였다.
    모든 장소들의 좌표를 리스트에 저장하고
    그 리스트들을 비교하여 두 점의 맨하탄 거리가 1000이하라면
    인접리스트를 이용하여 양방향으로 잇는 그래프를 만들었다.
    그리고 bfs를 이용하여 목적지까지 도착한다면 happy를 아니라면 sad를 출력해주었따.

--- gram/src/Baek9205.java
+++ gram/src/Baek9205.java
@@ -1,30 +1,66 @@
-//import java.io.*;
-//import java.util.*;
-//
-////class Pair{
-////	int x , y;
-////	Pair(int x,  int y){
-////		this.x=x;
-////		this.y=y;
-////	}
-////}
-//public class Baek9205 {
-//	public static int[] home = new int[2];
-//	public static int beer =20;
-//	public static void main(String[] args)throws IOException {
-//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//		int T = Integer.parseInt(br.readLine());
-//		for(int t=0;t<T;t++) {
-//			int n = Integer.parseInt(br.readLine());
-//			StringTokenizer st = new StringTokenizer(br.readLine());
-//			home[0]=Integer.parseInt(st.nextToken());
-//			home[1]=Integer.parseInt(st.nextToken());
-//			for(int i=0;i<n;i++) {
-//				st = new StringTokenizer(br.readLine());
-//			}
-//		}
-//		
-//		
-//	}
-//
-//}
+import java.io.*;
+import java.util.*;
+
+class Pair9205 {
+	int x, y;
+
+	Pair9205(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+}
+
+public class Baek9205 {
+	static ArrayList<Pair9205> list;
+	static ArrayList<ArrayList<Integer>> map;
+	static Queue<Integer> q ;
+	static boolean visited[];
+	static int destination;
+	static boolean tf;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		for (int t = 0; t < T; t++) {
+			int n = Integer.parseInt(br.readLine());
+			destination = n+1;
+			visited=new boolean[n+2];
+			list = new ArrayList<>();
+			map = new ArrayList<>();
+			q=new LinkedList<>();
+			tf=false;
+			for (int i = 0; i < n + 2; i++) {
+				StringTokenizer st = new StringTokenizer(br.readLine());
+				list.add(new Pair9205(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
+				map.add(new ArrayList<>());
+			}
+			for (int i = 0; i < list.size(); i++) {
+				for (int j = i + 1; j < list.size(); j++) {
+					if (Math.abs(list.get(i).x - list.get(j).x) + Math.abs(list.get(i).y - list.get(j).y) <= 1000) {
+						map.get(i).add(j);
+						map.get(j).add(i);
+					}
+				}
+			}
+			visited[0]=true;
+			q.offer(0);
+			bfs();
+			System.out.println(tf ? "happy":"sad");
+		}
+	}
+
+	public static void bfs() {
+		while(!q.isEmpty()) {
+			int x =q.poll();
+			if(x==destination) {
+				tf=true;
+				break;
+			}
+			for(int c  : map.get(x)) {
+				if(!visited[c]) {
+					visited[c]=true;
+					q.offer(c);
+				}
+			}
+		}
+	}
+}
