666a71b56dfc2038825d00b76fb08b2c6400a353
성혁 강 <95line@naver.com>
2021-01-24 21:58:27 +0900

    프로그래머스 1단계 // 체육복 , 그리디
    1년 전쯤? 이 문제를 풀었었는데 그 당시에는 이중포문을 사용하여 O(N^2)으로 풀었다.
    어쩌다가 다시 풀게되었는데 O(N)에 풀 수 있었다.
    생각해보면 O(N^2)로 푸는게 더 복잡해보이는데
    1년 전에는 자료구조를 잘 쓸줄 몰라서 그랬나보다.

--- gram/src/체육복.java
+++ gram/src/체육복.java
@@ -39,5 +39,41 @@ public class 체육복 {
 	public static void main(String[] args) {
 		solution(5,new int[] {2,4},new int[] {1,3,5});//5
 	}
-
 }
+
+
+/*
+ public static int solution(int n , int[] lost , int[] reserve){
+		int answer= n - lost.length;;
+		int count = 0;
+		int lost1=0;
+		for(int i = 0 ;i<reserve.length;i++){
+			for(int j = 0;j<lost.length;j++){
+				if(lost[j]==-10&&reserve[i]==-10)
+					continue;
+				if(lost[j]==reserve[i]){
+					lost[j]=-10;
+					reserve[i]=-10;
+					answer++;
+				}
+					
+			}
+		}
+		for(int i = 0 ;i<lost.length;i++){
+			for(int j = 0 ; j<reserve.length;j++){
+				if(lost[i]==-10||reserve[j]==-10)
+					continue;
+				if(reserve[j]+1==lost[i]||reserve[j]-1==lost[i]){
+					count++;
+					reserve[j]=-1;
+					break;
+				}
+			}
+		}
+		answer = answer+count+lost1;
+		if(answer>n){
+			answer=n;
+		}
+		return answer ;
+	}
+ */
