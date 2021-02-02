80ad81680d691d419c0caeeacbe76eaea59ba1d1
성혁 강 <95line@naver.com>
2021-02-02 19:29:09 +0900

    백준 1520 // 내리막 길 , DFS+DP
    (0,0)에서 DFS를 이용하여 (M-1,N-1)까지 도착하게 되면 그간 지나온 길을
    지나온 길의 원래 값 + 바로 이전에 도착했던 길을 더해주면 풀 수 있다.

--- gram/src/Baek1520.java
+++ gram/src/Baek1520.java
@@ -8,41 +8,47 @@ public class Baek1520 {
 	static int[] ny = { 1, 0, -1, 0 };
 	static int N;
 	static int M;
+	static boolean[][] visited;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		M = Integer.parseInt(st.nextToken());
 		N = Integer.parseInt(st.nextToken());
 		arr = new int[M][N];
 		dp = new int[M][N];
+		visited = new boolean[M][N];
 		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < N; j++) {
 				arr[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-		dp[0][0] = 0;
-
-		System.out.println(dfs(0, 0));
+		for (int i = 0; i < M; i++)
+			Arrays.fill(dp[i], -1);
+		dfs(0, 0);
+		bw.write(dp[0][0] + "");
+		bw.flush();
+		bw.close();
+		br.close();
 	}
 
 	public static int dfs(int x, int y) {
-//		System.out.println(x+" "+y);
-//		print();
 		if (x == M - 1 && y == N - 1)
 			return 1;
-		if (dp[x][y] != 0) 
+		if (dp[x][y] != -1) {
 			return dp[x][y];
-		for (int i = 0; i < nx.length; i++) {
+		}
+		int ret = 0;
+		for (int i = 0; i < 4; i++) {
 			int dx = nx[i] + x;
 			int dy = ny[i] + y;
 			if (isPossible(dx, dy) && arr[x][y] > arr[dx][dy]) {
-				dp[x][y] += dfs(dx, dy);
+				ret += dfs(dx, dy);
 			}
 		}
-
-		return dp[x][y];
+		return dp[x][y] = ret;
 	}
 
 	public static void print() {
