5a09719a0980ab36b650937e8ded5a6a8d4d9658
성혁 강 <95line@naver.com>
2021-02-04 16:18:07 +0900

    백준 1436 // 영화감독 숌 , 완전탐색
    그냥 완탐

--- gram/src/Baek1436.java
+++ gram/src/Baek1436.java
@@ -1,12 +1,23 @@
 import java.util.*;
+
 public class Baek1436 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		
-		for(int i =1;i<=n;i++) {
-			
+		ArrayList<Integer> list = new ArrayList<>();
+		list.add(0);
+		for (int i = 666; i <= 150000000; i++) {
+			String X = String.valueOf(i);
+			if (X.contains("666")) {
+				list.add(Integer.parseInt(X));
+				if(list.size()-1==n) {
+					System.out.println(list.get(n));
+					break;
+				}
+					
+			}
 		}
+
 	}
 
 }
