46acf7ac22a4e17e12d73a55a59b07021fbc6876
성혁 강 <95line@naver.com>
2021-02-07 15:49:20 +0900

    백준 16236 // 아기 상어 ,SWEA A형 기출 , bfs , 완전탐색
    현재 상어의 크기에서 먹을 수 있는 모든 물고기를 bfs를 이용하여 탐색하고 list에 저장한 후
    문제의 조건에 맞게 리스트를 정렬한다.
    그리고 첫번째 인덱스 값의 좌표를 큐에 집어 넣어 bfs를 반복하여 실행하고
    list에 아무런 값도 들어가있지 않다면 먹을 수 있는 물고기가 하나도 없는 것이므로 그때의 시간을 출력한다.

--- gram/src/Baek16236.java
+++ gram/src/Baek16236.java
@@ -1,139 +1,118 @@
 import java.util.*;
 import java.io.*;
 
-class Pair16236{
-	int x,y,level,eat,time;
-	
-	Pair16236(int x, int y,int level,int e,int t){
-		this.x=x;
-		this.y=y;
-		this.level=level;
-		this.eat=e;
-		this.time=t;
+class Point16236 {
+	int x;
+	int y;
+	int d;
+
+	public Point16236(int x, int y, int d) {
+		this.x = x;
+		this.y = y;
+		this.d = d;
 	}
-	
 }
 
 public class Baek16236 {
 	static int N;
-	static int[][] arr;
-	static int eat=0;
-	static int[][] map;
-	static boolean[][] visited;
-	static int[] nx= {0,-1,1,0};
-	static int[] ny= {-1,0,0,1};
-	static ArrayList<Pair16236> list = new ArrayList<Pair16236>();
-	static Queue<Pair16236> q = new LinkedList<>();
-	static HashMap<Integer,Integer> hsmap =new HashMap<>();
-	public static int stoi(String x) {return Integer.parseInt(x);}
-	public static void main(String[] args)throws IOException {
-		BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+	static int arr[][];
+	static boolean visited[][];
+	static int[] nx = { -1, 0, 1, 0 };
+	static int[] ny = { 0, -1, 0, 1 };
+	static int time = 0;
+	static int eat = -1;
+	static int size = 2;
+	static Queue<Point16236> q = new LinkedList<>();
+	static ArrayList<Point16236> list = new ArrayList<>();
+
+	public static int stoi(String x) {
+		return Integer.parseInt(x);
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		N = stoi(br.readLine());
 		arr = new int[N][N];
-		map=new int[N][N];
-		visited=new boolean[N][N];
-		for(int i =0;i<N;i++) {
-			StringTokenizer st= new StringTokenizer(br.readLine());
-			for(int j =0;j<N;j++) {
-				arr[i][j]=stoi(st.nextToken());
-				if(arr[i][j]==9) {
-					visited[i][j]=true;
-					arr[i][j]=0;
-					q.offer(new Pair16236(i,j,2,0,0));
-				}
-				if(arr[i][j]>=1&&arr[i][j]<=6) {
-					hsmap.put(arr[i][j],hsmap.getOrDefault(arr[i][j], 0)+1);
+		visited = new boolean[N][N];
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
+				arr[i][j] = stoi(st.nextToken());
+				if (arr[i][j] == 9) {
+					arr[i][j] = 0;
+					list.add(new Point16236(i, j, 0));
 				}
 			}
 		}
-		System.out.println(hsmap);
-		bfs();
+		while (true) {
+			if (list.size() < 1) {
+				bw.write(time + "");
+				break;
+			}
+			Collections.sort(list, new Comparator<Point16236>() {
+				@Override
+				public int compare(Point16236 arg0, Point16236 arg1) {
+					if (arg0.d < arg1.d) {
+						return -1;
+					} else if (arg0.d == arg1.d) {
+						if (arg0.x < arg1.x)
+							return -1;
+						else if (arg0.x == arg1.x) {
+							if (arg0.y < arg1.y)
+								return -1;
+							else
+								return 1;
+						} else
+							return 1;
+					} else
+						return 1;
+				}
+			});
+			q = new LinkedList<>();
+			if (eat + 1 == size) {
+				size++;
+				eat = 0;
+			} else {
+				eat++;
+			}
+			time += list.get(0).d;
+			q.offer(new Point16236(list.get(0).x, list.get(0).y, 0));
+			arr[list.get(0).x][list.get(0).y] = 0;
+			visited = new boolean[N][N];
+			visited[list.get(0).x][list.get(0).y] = true;
+			list = new ArrayList<>();
+			bfs();
+		}
+		bw.flush();
+		bw.close();
+		br.close();
 	}
+
+	public static void bfs() {
+		while (!q.isEmpty()) {
+			Point16236 p = q.poll();
+			int x = p.x;
+			int y = p.y;
+			int t = p.d;
+			for (int i = 0; i < 4; i++) {
+				int dx = nx[i] + x;
+				int dy = ny[i] + y;
+				if (isPossible(dx, dy) && !visited[dx][dy] && arr[dx][dy] <= size) {
+					if (arr[dx][dy] < size && arr[dx][dy] != 0) {
+						list.add(new Point16236(dx, dy, t + 1));
+					}
+					q.offer(new Point16236(dx, dy, t + 1));
+					visited[dx][dy] = true;
+				}
+			}
+		}
+	}
+
 	public static boolean isPossible(int x, int y) {
-		if(x<0||x>=N||y<0||y>=N)
+		if (x < 0 || x >= N || y < 0 || y >= N)
 			return false;
 		return true;
 	}
-	
-	public static void bfs() {
-		while(!q.isEmpty()) {
-			Pair16236 p = q.poll();
-			int x =p.x;
-			int y= p.y;
-			int l= p.level;
-			int e= p.eat;
-			int t= p.time;
-			boolean tf=false;
-			for(int i =1;i<l;i++) {
-				if(hsmap.get(i)!=null&&hsmap.get(i)!=0) {
-					tf=true;
-					break;
-				}
-			}
-			if(!tf) {
-				System.out.println(t);
-				break;
-			}
-			for(int i =0;i<nx.length;i++) {
-				int mx = x+nx[i];
-				int my = y+ny[i];
-				if(isPossible(mx,my)&&arr[mx][my]<=l&&!visited[mx][my]) {
-					if(arr[mx][my]!=0&&arr[mx][my]<l) {
-						int temp=l;
-						int tempe=e;
-						tempe++;
-						if(temp==tempe) {
-							temp++;
-							tempe=0;
-						}
-						list.add(new Pair16236(mx,my,temp,tempe,t+1));
-					}else {
-						q.offer(new Pair16236(mx,my,l,e,t+1));
-						visited[mx][my]=true;
-					}
-				}
-			}
-			if(list.isEmpty())continue;
-			Collections.sort(list , new Comparator<Pair16236>() {
-				@Override
-				public int compare(Pair16236 arg0, Pair16236 arg1) {
-					System.out.println(arg0.x+ " "+arg1.x);
-					if(arg0.x==arg1.x) {
-						if(arg0.y>arg1.y) {
-							return 1;
-						}else
-							return -1;
-					}else if(arg0.x>arg1.x) {
-						return 1;
-					}else
-						return 0;
-				}
-			});
-			Pair16236 go = list.get(0);
-			q=new LinkedList<>();
-			q.offer(go);
-			int mx =q.peek().x;
-			int my =q.peek().y;
-			visited = new boolean[N][N];
-			System.out.println(mx+" " +my);
-			visited[mx][my]=true;
-			hsmap.put(arr[mx][my],hsmap.get(arr[mx][my])-1);
-			arr[mx][my]=0;
-			list=new ArrayList<>();
-		}
-	}
-	
-	static void print() {
-		for(int i =0;i<N;i++) {
-			for(int j =0;j<N;j++) {
-				if(map[i][j]<10) {
-					System.out.print(map[i][j]+"  ");
-				}else
-					System.out.print(map[i][j]+" ");
-			}
-			System.out.println();
-		}
-		System.out.println("--------------------");
-	}
-	
+
 }
