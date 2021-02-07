0b99a18d5351e905f741ba28c3fb5a5c30f7aab9
성혁 강 <95line@naver.com>
2021-02-07 19:09:04 +0900

    백준 17069 // 파이프 옮기기2 , dfs+dp
    기존의 17070번 문제에서 int형 dp를 long 타입으로 변경하면 풀림

--- gram/src/Baek17069.java
+++ gram/src/Baek17069.java
@@ -1,4 +1,61 @@
+import java.util.*;
+import java.io.*;
 
 public class Baek17069 {
+	static int N;
+	static int[][] arr;
+	static long[][][] dp;
 
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		arr = new int[N][N];
+		dp = new long[N][N][4];
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		System.out.println(dfs(0, 1, 1));
+	}
+
+	public static long dfs(int x, int y, int dir) {
+		if (x == N - 1 && y == N - 1) {
+			return 1;
+		}
+		if (dp[x][y][dir] != 0) {
+			return dp[x][y][dir];
+		}
+		if (dir == 1 || dir == 2) {
+			if (isPossible(x, y + 1) && arr[x][y + 1] != 1) {
+				dp[x][y][dir] += dfs(x, y + 1, 1);
+			}
+		}
+		if (isPossible(x + 1, y + 1) && arr[x + 1][y + 1] != 1 && arr[x + 1][y] != 1 && arr[x][y + 1] != 1) {
+			dp[x][y][dir] += dfs(x + 1, y + 1, 2);
+		}
+		if (dir == 3 || dir == 2) {
+			if (isPossible(x + 1, y) && arr[x + 1][y] != 1) {
+				dp[x][y][dir] += dfs(x + 1, y, 3);
+			}
+		}
+		return dp[x][y][dir];
+	}
+
+	public static void print(int dir) {
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr.length; j++) {
+				System.out.print(dp[i][j][dir] + " ");
+			}
+			System.out.println();
+		}
+		System.out.println("-------------------------");
+	}
+
+	public static boolean isPossible(int x, int y) {
+		if (x < 0 || x >= N || y < 0 || y >= N)
+			return false;
+		return true;
+	}
 }
