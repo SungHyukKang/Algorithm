898f448df63233f97feb1387b40f9b958edf6122
성혁 강 <95line@naver.com>
2021-02-14 18:16:27 +0900

    백준 1107 / 리모콘 , 완전탐색

--- gram/src/Baek1107.java
+++ gram/src/Baek1107.java
@@ -1,11 +1,46 @@
 import java.util.*;
 import java.io.*;
-public class Baek1107 {
 
-	public static void main(String[] args)throws IOException {
-		BufferedReader br=  new BufferedReader(new InputStreamReader(System.in));
-		String target =br.readLine();
-		
+public class Baek1107 {
+	static int size;
+	static int t;
+	static boolean[] visited;
+	static int min;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String target = br.readLine();
+		size = target.length();
+		t = Integer.parseInt(target);
+		int broken = Integer.parseInt(br.readLine());
+		visited = new boolean[10];
+		if (broken > 0) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for (int i = 0; i < broken; i++) {
+				int x = Integer.parseInt(st.nextToken());
+				visited[x] = true;
+			}
+		}
+		min = Math.abs(100 - t);
+		for (int i = 1; i <= target.length() + 1; i++) {
+			size = i;
+			dfs(0, "");
+		}
+		System.out.println(min);
 	}
 
+	public static void dfs(int depth, String val) {
+		if (depth == size && !val.equals("")) {
+			int ta = Integer.parseInt(val);
+			if (min > Math.abs(t - ta) + depth) {
+				min = Math.abs(t - ta) + depth;
+			}
+			return;
+		}
+		for (int i = 0; i < visited.length; i++) {
+			if (!visited[i]) {
+				dfs(depth + 1, val + i);
+			}
+		}
+	}
 }
