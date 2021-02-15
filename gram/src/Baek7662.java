01d5ef5dcc2fc0a149acf0c8600a5571fc4f5f4e
성혁 강 <95line@naver.com>
2021-02-15 16:34:32 +0900

    백준 7662 // 이중 우선순위 큐 , 트리 맵 , 자료구조
    TreeMap을 사용했다.

--- gram/src/Baek7662.java
+++ gram/src/Baek7662.java
@@ -1,27 +1,56 @@
 import java.util.*;
+import java.io.*;
 
-class Pair7662 extends PriorityQueue<Integer> implements Comparable<Pair7662>{
-	int x;
-	
-	public void pollLast() {
-		System.out.println(this.size());
-	}
-	
-	@Override
-	public int compareTo(Pair7662 arg0) {
-		return this.x<=arg0.x?-1:1;
-	}
-	
-}
 public class Baek7662 {
 
-	public static void main(String[] args) {
-		Pair7662 p =new Pair7662();
-		p.offer(0);
-		p.offer(1);
-		p.offer(2);
-		p.pollLast();
-		System.out.println(p);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int T = Integer.parseInt(br.readLine());
+		for (int t = 0; t < T; t++) {
+			int k = Integer.parseInt(br.readLine());
+			TreeMap<Integer, Integer> treeMap = new TreeMap<>();
+			for (int i = 0; i < k; i++) {
+				StringTokenizer st = new StringTokenizer(br.readLine());
+				String com = st.nextToken();
+				int X = Integer.parseInt(st.nextToken());
+				if (com.equals("I")) {
+					treeMap.put(X, treeMap.getOrDefault(X, 0) + 1);
+				} else {
+					if (X == -1) {
+						if (!treeMap.isEmpty()) {
+							int key = treeMap.firstKey();
+							if (treeMap.containsKey(key)) {
+								treeMap.put(key, treeMap.get(key) - 1);
+								if (treeMap.get(key) == 0) {
+									treeMap.remove(key);
+								}
+							}
+						}
+					} else {
+						if (!treeMap.isEmpty()) {
+							int key = treeMap.lastKey();
+							if (treeMap.containsKey(key)) {
+								treeMap.put(key, treeMap.get(key) - 1);
+								if (treeMap.get(key) == 0) {
+									treeMap.remove(key);
+								}
+							}
+						}
+					}
+				}
+			}
+			if (treeMap.isEmpty()) {
+				bw.write("EMPTY\n");
+			} else {
+				bw.write(treeMap.lastKey() + " ");
+				bw.write(treeMap.firstKey() + "");
+				bw.write("\n");
+			}
+		}
+		bw.flush();
+		bw.close();
+		br.close();
 	}
 
 }
