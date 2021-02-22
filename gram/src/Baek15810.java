7fd29b6c1ed8c38c1144d54fbcaefcbda2eda579
성혁 강 <95line@naver.com>
2021-02-23 01:16:55 +0900

    백준 15810 //풍선 공장 , 이분탐색

---
+++ gram/src/Baek15810.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.io.*;
+
+public class Baek15810 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		long M = Integer.parseInt(st.nextToken());
+		long[] arr = new long[N];
+		long max = 0;
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+			if (max < arr[i])
+				max = arr[i];
+		}
+		long start = 1;
+		long end = max * M;
+		long ans = max * M;
+		while (start <= end) {
+			long mid = (start + end) / 2;
+			long sum = 0;
+			for (int i = 0; i < N; i++) {
+				sum += mid / arr[i];
+				if (sum >= M)
+					break;
+			}
+			if (sum >= M) {
+				ans = Math.min(ans, mid);
+				end = mid - 1;
+			} else
+				start = mid + 1;
+		}
+
+		System.out.println(ans);
+	}
+
+}
