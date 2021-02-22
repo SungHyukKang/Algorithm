a743858e6d464b93af95b097a6ecb7046e5100b5
성혁 강 <95line@naver.com>
2021-02-22 22:52:12 +0900

    백준 1072 // 게임 , 이분탐색

--- gram/src/Baek1072.java
+++ gram/src/Baek1072.java
@@ -1,28 +1,31 @@
 import java.util.*;
 import java.io.*;
+
 public class Baek1072 {
 
-	public static void main(String[] args)throws IOException {
-		BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		long X = Long.parseLong(st.nextToken());
 		long Y = Long.parseLong(st.nextToken());
-		long target = (Y*100/X)+1;
-		if(target==100 || target==101)
-			System.out.println("-1");
-		long cnt =0;
-		long left  = 1;
-		long right = 1000000000;
-		while(left<=right) {
-			long mid =(left+right)/2;
-			long val = (Y+mid)*100/(X+mid);
-			if(val>=target) {
-				right=mid-1;
-				cnt=mid;
-			}else
-				left=mid+1;
+		long target = (Y * 100 / X) + 1;
+		long cnt = 0;
+		long start = 1;
+		long end = X;
+		long mid = 0;
+		while (start <= end) {
+			mid = (start + end) / 2;
+			long pan = X + mid;
+			long win = Y + mid;
+			if (target <= (win * 100 / pan)) {
+				end = mid - 1;
+				cnt = mid;
+			} else {
+				start = mid + 1;
+			}
 		}
-		System.out.println(cnt);
+
+		System.out.println(cnt == 0 ? "-1" : cnt);
 	}
 
 }
