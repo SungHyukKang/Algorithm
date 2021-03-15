c5cdb3c7f5b1085e23f23137de60fe5268faab79
성혁 강 <95line@naver.com>
2021-03-16 01:39:39 +0900

    백준 2253 // 점프 , DFS + DP

--- gram/src/Baek2253.java
+++ gram/src/Baek2253.java
@@ -5,10 +5,10 @@ public class Baek2253 {
 	static int N;
 	static int M;
 	static int[] arr;
-	static int[] dp;
-	static int[] move = { 1, 0, -1 };
+	static int[][] dp;
+	static int INF = 987654321;
 	static int answer = 210000000;
-	static int INF = 21000000;
+	static boolean tf = false;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -16,39 +16,44 @@ public class Baek2253 {
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		arr = new int[N + 1];
-		dp = new int[N + 1];
+		dp = new int[250][N + 1];
 		for (int i = 0; i < M; i++) {
 			arr[Integer.parseInt(br.readLine())] = -1;
 		}
-		System.out.println(dfs(1, 1));
+		for (int i = 0; i < 250; i++)
+			Arrays.fill(dp[i], -1);
+		int x = dfs(1, 0);
+		System.out.println(tf ? x : "-1");
 	}
 
-	static int dfs(int m, int x) {
-		System.out.println(x+" " +m);
-		print();
-		if (x == N) {
+	public static int dfs(int pre, int m) {
+		if (pre == N) {
+			tf = true;
 			return 0;
 		}
-		if (dp[x] != 0) {
-			return dp[x];
+		if (dp[m][pre] != -1)
+			return dp[m][pre];
+
+		dp[m][pre] = INF;
+
+		if (pre + m - 1 <= N && m - 1 > 0 && arr[pre + m - 1] != -1) {
+			dp[m][pre] = Math.min(dp[m][pre], dfs(pre + m - 1, m - 1) + 1);
 		}
-		dp[x]=INF;
-		for (int i = 0; i < move.length; i++) {
-			int mm = move[i] + m;
-			if (mm >= 1) {
-				int nx = x + mm;
-				if ( nx <= N&&arr[nx] != -1 )
-					dp[x] = Math.min(dp[x], 1+dfs(mm, nx));
-			}
+
+		if (pre + m <= N && m > 0 && arr[pre + m] != -1) {
+			dp[m][pre] = Math.min(dp[m][pre], dfs(pre + m, m) + 1);
 		}
-		return dp[x];
+
+		if (pre + m + 1 <= N && m + 1 > 0 && arr[pre + m + 1] != -1) {
+			dp[m][pre] = Math.min(dp[m][pre], dfs(pre + m + 1, m + 1) + 1);
+		}
+
+		return dp[m][pre];
 	}
 
-	public static void print() {
-		for (int i = 1; i <= N; i++) {
-			System.out.print(dp[i] + " ");
-		}
-		System.out.println();
-		System.out.println("----------------------");
+	public static boolean isPossible(int pre) {
+		if (pre < 0 || pre >= N)
+			return false;
+		return true;
 	}
 }
