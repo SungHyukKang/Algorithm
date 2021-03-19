bd6c88947760e423250c5138c81131bf884fcd75
성혁 강 <95line@naver.com>
2021-03-19 16:15:24 +0900

    백준 11053 // 가장 긴 증가하는 부분 수열 4 , LIS

--- gram/src/Baek11053.java
+++ gram/src/Baek11053.java
@@ -8,21 +8,36 @@ public class Baek11053 {
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int[] S = new int[N + 1];
 		int[] dp = new int[N + 1];
+		int[] path = new int[N + 1];
 		for (int i = 1; i <= N; i++) {
 			S[i] = Integer.parseInt(st.nextToken());
 		}
-		int max=0;
+		int max = 0;
+		int re = 0;
 		for (int i = 1; i <= N; i++) {
-			dp[i]=1;
-			for(int j =1	;j<i ;j++) {
-				if(S[j]<S[i]&&dp[j]>=dp[i]) {
-					dp[i]=dp[j]+1;
+			dp[i] = 1;
+			for (int j = 1; j < i; j++) {
+				if (S[j] < S[i] && dp[j] >= dp[i]) {
+					dp[i] = dp[j] + 1;
+					path[i] = j;
 				}
 			}
-			if(max<dp[i])
-				max=dp[i];
+			if (max < dp[i]) {
+				max = dp[i];
+				re = i;
+			}
 		}
-		System.out.println(max);
+		StringBuilder sb = new StringBuilder();
+		Stack<Integer> stack = new Stack<>();
+		sb.append(max + "\n");
+		stack.push(S[re]);
+		for (int i = re; path[i] != 0; i = path[i]) {
+			stack.push(S[path[i]]);
+		}
+		while (!stack.isEmpty()) {
+			sb.append(stack.pop() + " ");
+		}
+		System.out.println(sb);
 	}
 
 }
