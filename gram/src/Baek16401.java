aa1778069b8b74125e97442e470a04ecabf386aa
성혁 강 <95line@naver.com>
2021-03-24 23:52:50 +0900

    백준 16401 // 과자 나눠주기 , 이분탐색

---
+++ gram/src/Baek16401.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.io.*;
+
+public class Baek1034 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int[] arr = new int[M];
+		st = new StringTokenizer(br.readLine());
+		int left = 1;
+		int right = 0;
+		int ans = 0;
+		for (int i = 0; i < M; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+			right = Math.max(right, arr[i]);
+		}
+
+		while (left <= right) {
+			int mid = (left + right) / 2;
+			int cnt = 0;
+			for (int i = 0; i < M; i++) {
+				if (arr[i] >= mid) {
+					cnt += arr[i] / mid;
+				}
+				if(cnt>=N)
+					break;
+			}
+
+			if (cnt >= N) {
+				left = mid + 1;
+				ans = Math.max(mid, ans);
+			} else {
+				right = mid - 1;
+			}
+		}
+
+		System.out.println(ans);
+	}
+
+}
