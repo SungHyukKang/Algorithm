2a4d64a101e6a34e66be56547d9784dba3a75409
성혁 강 <95line@naver.com>
2021-05-04 19:40:10 +0900

    백준 2240 // 자두나무 , DP(Top-Down)

--- gram/src/Baek2240.java
+++ gram/src/Baek2240.java
@@ -3,7 +3,6 @@ import java.io.*;
 
 public class Baek2240 {
 	static int[] arr;
-	static boolean[] visited;
 	static int[][] dp;
 	static int N;
 	static int W;
@@ -14,11 +13,38 @@ public class Baek2240 {
 		N = Integer.parseInt(st.nextToken());
 		W = Integer.parseInt(st.nextToken());
 		arr = new int[N];
-		visited = new boolean[N];
 		for (int i = 0; i < N; i++) {
 			arr[i] = Integer.parseInt(br.readLine());
 		}
+		dp = new int[1001][31];
+		for (int i = 0; i < 1001; i++) {
+			Arrays.fill(dp[i], -1);
+		}
+		int test1 = dfs(0, W, 1);
+		for (int i = 0; i < 1001; i++) {
+			Arrays.fill(dp[i], -1);
+		}
+		int test2 = dfs(0, W - 1, 2);
+		System.out.println(Math.max(test1, test2));
 
+	}
+
+	public static int dfs(int index, int count, int pre) {
+		if (index == N) {
+			return 0;
+		}
+
+		if (dp[index][count] != -1) {
+			return dp[index][count];
+		}
+		dp[index][count] = 0;
+		int jadu = arr[index] == pre ? 1 : 0;
+		dp[index][count] = Math.max(dp[index][count], dfs(index + 1, count, pre) + jadu);
+		if (count > 0) {
+			dp[index][count] = Math.max(dp[index][count], dfs(index + 1, count - 1, 3 - pre) + jadu);
+		}
+
+		return dp[index][count];
 	}
 
 }
