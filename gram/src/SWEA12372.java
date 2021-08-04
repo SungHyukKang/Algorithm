ce148810ffdaf39442f8a6ab06d809fc8f396b68
성혁 강 <95line@naver.com>
2021-08-04 16:45:35 +0900

    SWEA 12372 삼성 하계 알고리즘 특강 // Partial Sort , Heap
    heap 자료구조 구현

---
+++ gram/src/SWEA12372.java
@@ -0,0 +1,166 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+class UserSolution12372 {
+	int[][] users;
+	int userCount;
+
+	public void init() {
+		users = new int[100001][2];
+		userCount = 0;
+	}
+
+	public void addUser(int uID, int income) {
+
+		users[userCount][0] = uID;
+		users[userCount][1] = income;
+		int current = userCount;
+		while (current > 0 && users[current][1] > users[(current - 1) / 2][1]) {
+			if (users[current][1] == users[(current - 1) / 2][1]) {
+				if (users[current][0] < users[(current - 1) / 2][0]) {
+					int tId = users[(current - 1) / 2][0];
+					int tIc = users[(current - 1) / 2][1];
+					users[(current - 1) / 2][0] = users[current][0];
+					users[(current - 1) / 2][1] = users[current][1];
+					users[current][0] = tId;
+					users[current][1] = tIc;
+					current = (current - 1) / 2;
+				}
+			} else {
+				int tId = users[(current - 1) / 2][0];
+				int tIc = users[(current - 1) / 2][1];
+				users[(current - 1) / 2][0] = users[current][0];
+				users[(current - 1) / 2][1] = users[current][1];
+				users[current][0] = tId;
+				users[current][1] = tIc;
+				current = (current - 1) / 2;
+			}
+		}
+		userCount++;
+	}
+
+	int[] pop(int[][] heap) {
+		int banbok = userCount >= 10 ? 10 : userCount;
+		int[] result = new int[10];
+		int heapSize = userCount;
+		for (int i = 0; i < banbok; i++) {
+			int value = heap[0][0];
+//			System.out.print(heap[0][1] + " ");
+			result[i] = value;
+			heapSize--;
+			heap[0][0] = heap[heapSize][0];
+			heap[0][1] = heap[heapSize][1];
+			int current = 0;
+			while (current * 2 + 1 < heapSize) {
+				int child;
+				if (current * 2 + 2 == heapSize) {
+					child = current * 2 + 1;
+				} else {
+					if (heap[current * 2 + 1][1] == heap[current * 2 + 2][1]) {
+						child = heap[current * 2 + 1][0] < heap[current * 2 + 2][0] ? current * 2 + 1 : current * 2 + 2;
+					} else {
+						child = heap[current * 2 + 1][1] > heap[current * 2 + 2][1] ? current * 2 + 1 : current * 2 + 2;
+					}
+				}
+				if (heap[current][1] > heap[child][1])
+					break;
+				int tempId = heap[current][0];
+				int tempIc = heap[current][1];
+				heap[current][0] = heap[child][0];
+				heap[current][1] = heap[child][1];
+				heap[child][0] = tempId;
+				heap[child][1] = tempIc;
+				current = child;
+			}
+		}
+//		System.out.println();
+//		for (int i = 0; i < banbok; i++) {
+//			System.out.print(result[i] + " ");
+//		}
+//		System.out.println();
+		return result;
+	}
+
+	int getTop10(int[] result) {
+		int[][] tUsers = new int[userCount][2];
+		for (int i = 0; i < userCount; i++) {
+			tUsers[i][0] = users[i][0];
+			tUsers[i][1] = users[i][1];
+		}
+		int[] cpy = pop(tUsers);
+		for (int i = 0; i < 10; i++) {
+			result[i] = cpy[i];
+		}
+		return userCount >= 10 ? 10 : userCount;
+	}
+}
+
+public class SWEA12372 {
+
+	private final static int MAX_INPUT = 100000;
+	private final static int MAX_NUM = 30000;
+
+	private final static UserSolution12372 usersolution = new UserSolution12372();
+
+	private static BufferedReader br;
+
+	private static int[] input = new int[MAX_INPUT];
+	private static long seed = 13410;
+
+	private static long pseudoRand() {
+		seed = (seed * 214013 + 2531011) & 0xffffffffL;
+		return (seed >> 11) % MAX_NUM;
+	}
+
+	private static void makeInput(int inputLen) {
+		for (int i = 0; i < inputLen; i++) {
+			input[i] = (int) (pseudoRand());
+		}
+	}
+
+	private static int run() throws Exception {
+		int score = 100;
+		int N, userNum, uID = 0, ret, sum, ans;
+		int[] result = new int[10];
+		String str;
+
+		str = br.readLine();
+		N = Integer.parseInt(str);
+
+		for (int i = 0; i < N; i++) {
+			str = br.readLine();
+			userNum = Integer.parseInt(str);
+			makeInput(userNum);
+
+			for (int j = 0; j < userNum; j++) {
+				usersolution.addUser(uID++, input[j]);
+			}
+			ret = usersolution.getTop10(result);
+
+			sum = 0;
+			for (int j = 0; j < ret; j++) {
+				sum += result[j];
+			}
+			str = br.readLine();
+			ans = Integer.parseInt(str);
+			if (sum != ans) {
+				score = 0;
+			}
+		}
+		return score;
+	}
+
+	public static void main(String[] args) throws Exception {
+		int TC;
+		// System.setIn(new java.io.FileInputStream("res/sample_input.txt"));
+
+		br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+		TC = Integer.parseInt(str);
+
+		for (int tc = 1; tc <= TC; tc++) {
+			usersolution.init();
+			System.out.println("#" + tc + " " + run());
+		}
+	}
+}
