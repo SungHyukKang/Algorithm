4ec146f16bc6af0ff42fac4770aed33671f301c8
성혁 강 <95line@naver.com>
2021-10-03 21:53:01 +0900

    백준 2665 // 미로만들기 , 다익스트라

--- gram/src/Baek2665.java
+++ gram/src/Baek2665.java
@@ -1,80 +1,70 @@
 import java.util.*;
 import java.io.*;
 
-//class Node3 implements Comparable<Node3>{
-//	int i;
-//	int j;
-//	int dist;
-//	
-//	
-//	Node3(int i , int j ,int dist){
-//		this.i=i;
-//		this.j=j;
-//		this.dist=dist;
-//	}
-//	@Override
-//	public int compareTo(Node3 arg0) {
-//		return this.dist<=arg0.dist ? -1 : 1;
-//	}
-//}
-
-
 public class Baek2665 {
 	static int N;
-	static int[][] arr;
-	static int[][] D;
-	static final int INF =999;
-	static int[] nx= {0,0,1,-1};
-	static int[] ny= {1,-1,0,0};
-	public static int stoi(String X) {return Integer.parseInt(X);}
-	public static void main(String[] args)throws IOException {
+
+	static class Node implements Comparable<Node> {
+		int x, y, d;
+
+		Node(int x, int y, int d) {
+			this.x = x;
+			this.y = y;
+			this.d = d;
+		}
+
+		public int compareTo(Node arg0) {
+			return this.d - arg0.d;
+		}
+
+	}
+
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = stoi(br.readLine());
-		arr=new int[N][N];
-		D=new int[N][N];
-		for(int i =0;i<N;i++) {
-			String X = br.readLine();
-			int j =0;
-			for(String Z : X.split("")) {
-				arr[i][j]=stoi(Z);
-				D[i][j]=INF;
-				j++;
+		N = Integer.parseInt(br.readLine());
+		int[][] map = new int[N][N];
+		for (int i = 0; i < N; i++) {
+			String str = br.readLine();
+			for (int j = 0; j < N; j++) {
+				map[i][j] = str.charAt(j) - '0';
 			}
 		}
-		dijkstra();
-		System.out.println(D[N-1][N-1]);
-		
-	}
-	public static void dijkstra() {
-		PriorityQueue<Node3> pq =new PriorityQueue<>();
-		pq.offer(new Node3(0,0,0));
-		D[0][0]=0;
-		while(!pq.isEmpty()) {
-			Node3 node = pq.poll();
-			int x = node.i;
-			int y = node.j;
-			for(int i =0;i<nx.length;i++) {
-				if(isPossible(x+nx[i],y+ny[i])) {
-					if(arr[x+nx[i]][y+ny[i]]==0) {
-						if(D[x+nx[i]][y+ny[i]]>D[x][y]+1) {
-							D[x+nx[i]][y+ny[i]]=D[x][y]+1;
-							pq.offer(new Node3(x+nx[i],y+ny[i],D[x+nx[i]][y+ny[i]]));
-						}
-					}else {
-						if(D[x+nx[i]][y+ny[i]]>D[x][y]) {
-							D[x+nx[i]][y+ny[i]]=D[x][y];
-							pq.offer(new Node3(x+nx[i],y+ny[i],D[x+nx[i]][y+ny[i]]));
-						}
+		int[][] D = new int[N][N];
+		for (int i = 0; i < N; i++)
+			Arrays.fill(D[i], Integer.MAX_VALUE);
+		PriorityQueue<Node> pq = new PriorityQueue<>();
+		pq.offer(new Node(0, 0, 0));
+		D[0][0] = 0;
+		int[] dx = { 0, 1, 0, -1 };
+		int[] dy = { 1, 0, -1, 0 };
+		while (!pq.isEmpty()) {
+			Node cur = pq.poll();
+			int x = cur.x;
+			int y = cur.y;
+			int d = cur.d;
+			if (x == N - 1 && y == N - 1) {
+				System.out.println(d);
+				return;
+			}
+			for (int i = 0; i < 4; i++) {
+				int nx = dx[i] + x;
+				int ny = dy[i] + y;
+				if (nx < 0 || nx >= N || ny < 0 || ny >= N)
+					continue;
+				if (D[nx][ny] <= D[x][y])
+					continue;
+				if (map[nx][ny] == 1) {
+					pq.offer(new Node(nx, ny, d));
+					D[nx][ny] = d;
+				} else {
+					if (D[nx][ny] > d + 1) {
+						D[nx][ny] = d + 1;
 					}
+					pq.offer(new Node(nx, ny, D[nx][ny]));
 				}
 			}
-			
+
 		}
 	}
-	public static boolean isPossible(int x, int y) {
-		if(x<0||x>=N||y<0||y>=N)
-			return false;
-		return true;
-	}
-	
+
 }
