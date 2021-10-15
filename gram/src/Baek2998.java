28b9d4ff81b309716915dce25df327b84690b89b
성혁 강 <95line@naver.com>
2021-10-15 20:27:01 +0900

    백준 2998 // 8진수 , 구현

--- gram/src/Baek2998.java
+++ gram/src/Baek2998.java
@@ -1,17 +1,31 @@
 import java.io.*;
+
 public class Baek2998 {
-	public static void main(String[] args)throws IOException {
-		BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder(br.readLine());
-		sb= sb.reverse();
-		int sum=0;
-		for(int i =0;i<sb.length();i++) {
-			sum+=Math.pow(2*i,Integer.parseInt(Character.toString(sb.charAt(i))));
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		sb.append(br.readLine()).reverse();
+		while (true) {
+			if (sb.length() % 3 == 0) {
+				break;
+			}
+			sb.append(0);
 		}
-		sb=new StringBuilder();
-		while(sum>0) {
-			sb.append(sum%8);
-			sum/=8;
+		sb = sb.reverse();
+		char[] chArr = sb.toString().toCharArray();
+		int decimal = 0;
+		int mod = 0;
+		sb = new StringBuilder();
+		for (int i = chArr.length - 1; i >= 0; i--) {
+			int cToi = chArr[i] - '0';
+			if (cToi == 1) {
+				decimal += (cToi << (mod % 3));
+			}
+			if ((mod + 1) % 3 == 0 && mod != 0) {
+				sb.append(decimal);
+				decimal = 0;
+			}
+			mod++;
 		}
 		System.out.println(sb.reverse());
 	}
