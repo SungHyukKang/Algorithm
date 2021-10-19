4708ad2240e2813a7137377ca34955983023bcf5
성혁 강 <95line@naver.com>
2021-10-19 17:48:58 +0900

    백준 1256 // 신기한 키보드 , 완탐

---
+++ gram/src/Baek1256.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.io.*;
+
+public class Baek1256 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+	}
+
+}
